29f7a7c5e90d12201094fbff2e9e6a985dcf74d4
NickMostovoi <nick211996@gmail.com>
2022-02-10 19:06:20 +0200

    rozetka: added slider

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-document.querySelector('.fn-burger').addEventListener('click', function() {
-    document.querySelector('.burger').classList.toggle('hidden');
+document.querySelector('.fn-header__burgerButton_open').addEventListener('click', function() {
+    document.querySelector('.asideMenu').style.display = 'block';
+});
+
+document.querySelector('.fn-button__radio_close').addEventListener('click', function() {
+    document.querySelector('.asideMenu').style.display = 'none';
 });
